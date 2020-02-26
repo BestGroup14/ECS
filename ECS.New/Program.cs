ff9fe49888ea0f9040d61348b4b330b558667743
carolinenissen <201806156@uni.au.dk>
2020-02-26 14:32:00 +0100

    Added cover

--- ECS.New/Program.cs
+++ ECS.New/Program.cs
@@ -20,6 +20,28 @@ namespace ECS.New
             ecs.SetThreshold(20);
 
             ecs.Regulate();
+
+            bool cont = true;
+            int _value = 0;
+            int _uppervalue = 0;
+
+            while (cont)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                char keyChar = keyInfo.KeyChar;
+                if (keyChar == 'a')
+                {
+                    _value = Convert.ToInt32(Console.ReadLine());
+                    ecs.SetThreshold(_value);
+                }
+
+                if (keyChar == 'b')
+                {
+                    _uppervalue = Convert.ToInt32(Console.ReadLine());
+                    ecs.SetUpperThreshold(_uppervalue);
+                }
+
+            }
         }
     }
 }
